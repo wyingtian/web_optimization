1b2a1e98d6f45c968700eac3fbb000790a71f27b
wyingtian <yingtianwang@g.harvard.edu>
2016-01-04 17:38:52 +0800

    Reorganize file structure

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+  
+  ////////////////////////////////////////////////////
+  // grunt and plugin configuration
+  ////////////////////////////////////////////////////
+  
+  grunt.initConfig({
+    
+    pkg: grunt.file.readJSON('package.json'),
+    
+    concat: {
+      options: { separator: ';' },
+      
+      js: {
+        src: ['src/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      } 
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+        mangle: false
+      },
+      js: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.js']
+        }
+      }
+    },
+
+    cssmin: {
+      target: {
+        files: { 'dist/style.min.css': ['css/style.css'] }
+      }
+    },
+  });
+  
+  ////////////////////////////////////////////////////
+  // Load all necessary grunt plugins
+  ////////////////////////////////////////////////////
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  ////////////////////////////////////////////////////
+  // Main grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('build', [
+    'concat',
+    'uglify',
+    'cssmin'
+  ]);
+  
+};
