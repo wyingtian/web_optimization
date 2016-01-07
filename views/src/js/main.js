49f372bdcefd53886ac9d68c7e9a6979b532759b
wyingtian <yingtianwang@g.harvard.edu>
2016-01-07 08:26:41 +0800

    optimize updatePosition function
    optimize updatePosition function

--- views/src/js/main.js
+++ views/src/js/main.js
@@ -506,10 +506,14 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
-
-  var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+   //make the scrollTop calculation out of the loop
+   //move variable declaration out of the loop
+  var items = document.querySelectorAll('mover');
+  var scrTop = (document.documentElement.scrollTop / 1250);
+  var phase;
+  var itemLen = items.length;
+  for (var i = 0 ; i < itemLen; i++) {
+    phase = Math.sin( scrTop + (i % 5));;
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
@@ -532,15 +536,17 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
+  var elem;
+  var movingPizzas = document.getElementById('movingPizzas1');
   for (var i = 0; i < 30; i++) {
-    var elem = document.createElement('img');
+    elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    movingPizzas.appendChild(elem);
   }
   updatePositions();
 });
